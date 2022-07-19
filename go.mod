acb1540b8dde56845c6f6252498e0c61dc937fc0
panutat-p <pp.panutat@gmail.com>
2022-07-20 00:42:51 +0700

    select interactive prompt

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/panutat-p/finn-thai-funds-go
 
 go 1.18
+
+require (
+	github.com/chzyer/readline v1.5.1 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -2,20 +2,40 @@ package main
 
 import (
 	"fmt"
+	"github.com/manifoldco/promptui"
 	"io"
 	"net/http"
 	"strings"
 )
 
 func main() {
-	r, _ := http.Get("https://storage.googleapis.com/finno-ex-re-v2-static-staging/recruitment-test/fund-ranking-1Y.json")
-	fmt.Println(r.Status)
-	fmt.Println(r.Header["Content-Type"])
+	for {
+		prompt := promptui.Select{
+			Label: "Select Time Range",
+			Items: []string{"1Y", "1M", "1W", "1D", "🟥"},
+		}
 
-	buf := new(strings.Builder)
-	_, err := io.Copy(buf, r.Body)
-	if err != nil {
-		panic(err)
+		_, result, err := prompt.Run()
+
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			break
+		}
+
+		if result == "🟥" {
+			break
+		} else {
+			fmt.Printf("You choose %q\n", result)
+			r, _ := http.Get("https://storage.googleapis.com/finno-ex-re-v2-static-staging/recruitment-test/fund-ranking-1Y.json")
+			fmt.Println(r.Status)
+			fmt.Println(r.Header["Content-Type"])
+
+			buf := new(strings.Builder)
+			_, err := io.Copy(buf, r.Body)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(buf.String())
+		}
 	}
-	fmt.Println(buf.String())
 }
